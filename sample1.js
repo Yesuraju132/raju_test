c11196bab1995718ce505265a07c2f2b7022c87c
Yesuraju132 <109262106+Yesuraju132@users.noreply.github.com>
2024-02-20 11:04:47 +0000

    first commit

---
+++ sample1.js
@@ -0,0 +1,9 @@
+// script.js
+function generateJSONResponse() {
+    // Generate or fetch the JSON data
+    var jsonData = { redrct:true };
+  
+    // Return the JSON response
+    return JSON.stringify(jsonData);
+  }
+  
